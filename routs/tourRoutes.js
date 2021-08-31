00e59b5e00d29b97274668bdeb4f08fba590bf36
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-31 15:52:08 +0300

    Get monthly plan path added

--- routs/tourRoutes.js
+++ routs/tourRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 //router.param('id', tourController.checkId);
 
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/top-5-cheap')
